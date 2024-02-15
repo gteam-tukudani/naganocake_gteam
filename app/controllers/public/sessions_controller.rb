9ec577d1fc6ba06626da900370cc0ae2d1d94688
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-02-15 13:56:46 +0000

    login logout

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
@@ -10,11 +8,6 @@ class Public::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
   #   super
   # end
 
