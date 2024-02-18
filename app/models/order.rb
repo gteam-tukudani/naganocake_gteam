b211a28a3c35fb987e8569491e2ee242f84b1b9c
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-02-18 07:12:46 +0000

    totyuu

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,7 @@
 class Order < ApplicationRecord
-
-
-#支払い方法
-enum payment_method: {credit_card: 0, transfer: 1}
-#注文ステータス
-enum status: {wait_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, finish_prepare: 4}
+  belongs_to :customer
+  #支払い方法
+  enum payment_method: {credit_card: 0, transfer: 1}
+  #注文ステータス
+  enum status: {wait_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, finish_prepare: 4}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2024_02_17_065154) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
-    t.boolean "is_active", null: false
+    t.boolean "is_active", default: true, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -117,14 +117,14 @@ ActiveRecord::Schema.define(version: 2024_02_17_065154) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "postal_code"
-    t.string "address"
-    t.string "name"
-    t.integer "shipping_cost"
-    t.integer "total_payment"
-    t.integer "payment_method"
-    t.integer "status"
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "payment_method", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
