ba26dd37222115a73a3d2cb5cb5555150deb8817
ferington <155619800+ferington@users.noreply.github.com>
2024-02-18 15:53:56 +0900

    fix 商品新規登録画面作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
   
+  has_one_attached :item_image
+  
   has_many :cart_items, dependent: :destroy
   has_many :oeder_details, dependent: :destroy
   has_many :orders, through: :ordr_details
   
   belongs_to :genre
   
-  has_one_attached :item_image
+  enum genre_method: { cake: 0, purine: 1, baked_sweets: 2, candy:3 }
   
 end
