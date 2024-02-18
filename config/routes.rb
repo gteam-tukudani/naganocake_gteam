1b275272f93ce4c5552992680f1110c559a9f1fb
ferington <155619800+ferington@users.noreply.github.com>
2024-02-18 16:28:01 +0900

    Committing changes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -71,10 +71,13 @@ Rails.application.routes.draw do
    get "customers/unsubscribe" => "customers#unsubscribe"
   # 論理的削除用のルーティング
    patch "customers/withdraw" => "customers#withdraw"
-
+   get "customers/mypage" => "customers/show"
+   get "customers/information/edit" => "customers/edit"
+   patch "customers/information" => "customers/update"
+   get "customers/unsubscribe" => "customers/unsubscribe"
+   patch "customers/withdraw" => "customers/withdraw"
    resources :items, only: [:index, :show]
    resources :orders, only: [:new, :create, :show, :index]
-     post "orders/confirm" => "orders#confirm"
    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
