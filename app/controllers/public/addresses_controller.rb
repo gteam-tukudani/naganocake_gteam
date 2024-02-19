cc986c24b8a78981cbce57e4f6f80297e1e7d320
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-02-19 10:20:24 +0000

    nannka

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,21 @@
 class Public::AddressesController < ApplicationController
-  
+
+  def index
+    @addresses = Address.all
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :index
+    end
+  end
+
   private
-  
+
   def address_params
     params.require(:address).permit(:name, :postal_code, :address)
   end
