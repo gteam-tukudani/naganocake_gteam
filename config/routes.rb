6ccdaed8521765a1accb22828369486b1fcc5242
nekoniumoretai <152590285+nekoniumoretai@users.noreply.github.com>
2024-02-20 04:46:38 +0000

    Bootstrap_install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,41 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  namespace :admin do
+    resources :order_details, only: [:update]
+  end
+  namespace :admin do
+    resources :orders, only: [:show, :update]
+  end
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+
+  end
+
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroy_aii'
+    get 'cart_items/create'
+  end
+
+  namespace :admin do
+    get "/" =>'homes#top'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroy_aii'
+    get 'cart_items/create'
+  end
 
   devise_for :customers, controllers: {
   registrations: "public/registrations",
@@ -9,10 +44,18 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
   sessions: "admin/sessions"
   }
+  
+  devise_scope :admin do
+    get '/admin/sign_out' => 'devise/sessions#destroy'
+  end
+  
+  devise_scope :customer do
+    get '/customers/sign_out' => 'devise/sessions#destroy'
+  end
 
   scope module: :public do
    root to: "homes#top"
-   get "homes/about" => "homes#about", as: 'about'
+   get "about" => "homes#about", as: 'about'
    get "customers/mypage" => "customers#show"
    get "customers/information/edit" => "customers#edit"
    patch "customers/information" => "customers#update"
@@ -25,9 +68,14 @@ Rails.application.routes.draw do
    patch "customers/information" => "customers/update"
    get "customers/unsubscribe" => "customers/unsubscribe"
    patch "customers/withdraw" => "customers/withdraw"
-   resources :item, only: [:index, :show]
-   resources :orders, only: [:new, :create, :show, :index]
+   resources :items, only: [:index, :show]
+   resources :orders, only: [:new, :create, :show, :index] do
+     get 'thanks', on: :collection
+   end
+     post "orders/confirm" => "orders#confirm"
    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+   resources :cart_items, only: [:index, :update, :destroy, :create]
+   delete "cart_items/destroy_all" => "cart_items#destroy_all"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
