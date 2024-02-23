171d8b6dc7ed29f49f469c33cc485d63caada9ae
ferington <155619800+ferington@users.noreply.github.com>
2024-02-23 13:01:06 +0900

    fix商品一覧画像サイズ調整3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -28,3 +28,4 @@
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number,])
   end
+end
