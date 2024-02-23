a8d2bd65bb7a2c008e2fda9a74cbcfd505eba249
Souax <126418531+Souax@users.noreply.github.com>
2024-02-23 03:48:28 +0000

    でぃているず

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,11 @@ class Order < ApplicationRecord
   enum payment_method: {credit_card: 0, transfer: 1}
   #注文ステータス
   enum status: {wait_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, finish_prepare: 4}
+  
+  
+  
+  
+  def is_all_order_details_making_completed?
+    order_details.all? { |detail| detail.making_status == 'completed' }
+  end
 end
