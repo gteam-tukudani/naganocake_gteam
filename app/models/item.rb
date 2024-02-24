c2e2861fb7df521fd4fb3f7dd7312a8a04ee4001
ferington <155619800+ferington@users.noreply.github.com>
2024-02-24 17:49:30 +0900

    fix デザイン調整ジャンル、会員一覧

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,8 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  has_many_attached :images 
   has_many :cart_items, dependent: :destroy
-  has_many :oeder_details, dependent: :destroy
+  has_many :order_details, dependent: :destroy
   has_many :orders, through: :ordr_details
 
   belongs_to :genre
